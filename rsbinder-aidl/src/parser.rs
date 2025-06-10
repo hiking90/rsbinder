7bd4486d7b0294b8187de172a012af5634073fac
Jeff Kim <hiking90@gmail.com>
2025-06-10 22:31:34 +0900

    Update linting directive to allow missing const for functions instead of thread local

--- rsbinder-aidl/src/parser.rs
+++ rsbinder-aidl/src/parser.rs
@@ -1,7 +1,7 @@
 // Copyright 2022 Jeff Kim <hiking90@gmail.com>
 // SPDX-License-Identifier: Apache-2.0
 
-#![allow(clippy::missing_const_for_thread_local)]
+#![allow(clippy::missing_const_for_fn)]
 
 use std::cell::RefCell;
 use std::collections::HashMap;
